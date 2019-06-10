ee9431e80efc25ea913984e661c6666b77b40733
Phillip Ahereza <ahereza@ahereza.local>
2019-06-10 20:05:52 +0300

    added test for errors returned on insufficient funds

--- pointers_errors/banking.go
+++ pointers_errors/banking.go
@@ -1,6 +1,41 @@
 package banking
 
+import (
+	"errors"
+	"fmt"
+)
+
+// ErrInsufficientFunds error
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+// Bitcoin of int
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // Wallet where money is kept
 type Wallet struct {
-	Balance int
+	balance Bitcoin
 }
+
+// Deposit adds money to the wallet
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += 10
+}
+
+// Balance gets wallet balance
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+// Withdraw removes some money
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+} 
--- pointers_errors/banking_test.go
+++ pointers_errors/banking_test.go
@@ -5,13 +5,48 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
 
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw with funds", func(t *testing.T){
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T){
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, Bitcoin(20))
+
+		assertError(t, err, ErrInsufficientFunds)
+	})
+
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
-	want := 10
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("wanted an error but didn't get one")
+	}
+
+	if got.Error() != want.Error() {
+		t.Errorf("got '%s', want '%s'", got, want)
 	}
 }
